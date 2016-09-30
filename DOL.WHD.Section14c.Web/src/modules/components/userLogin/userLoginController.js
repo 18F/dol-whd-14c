07496abe9137f5d8b476d2fec88e0e29eff4f077
Jeff Smith <jeffsmith@gmail.com>
2016-09-30 09:27:43 -0400

    Fixed dangling comma.

--- DOL.WHD.Section14c.Web/src/modules/components/userLogin/userLoginController.js
+++ DOL.WHD.Section14c.Web/src/modules/components/userLogin/userLoginController.js
@@ -10,7 +10,7 @@ module.exports = function(ngModule) {
 
         $scope.formVals = {
             'email': '',
-            'pass': '',
+            'pass': ''
         };
 
         $scope.onSubmitClick = function() {
