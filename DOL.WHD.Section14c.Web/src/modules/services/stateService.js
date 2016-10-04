8012305da10ed73b8f61159a32026a1ca5ddc969
Jeff Smith <jeffsmith@gmail.com>
2016-10-04 11:10:12 -0400

    Small update to data model.

--- DOL.WHD.Section14c.Web/src/modules/services/stateService.js
+++ DOL.WHD.Section14c.Web/src/modules/services/stateService.js
@@ -1,11 +1,16 @@
 'use strict';
 
+import merge from 'lodash/merge'
+
 module.exports = function(ngModule) {
     ngModule.service('stateService', function() {
         'use strict';
 
         let state = {
+            form_data: { }
         };
+
+        /*** Properties ***/
 
         // email
         Object.defineProperty(this, 'email', {
@@ -13,9 +18,26 @@ module.exports = function(ngModule) {
             set: function(value) { state.email = value; }
         });
 
+        // REST access token
         Object.defineProperty(this, 'access_token', {
             get: function() { return state.access_token; },
             set: function(value) { state.access_token = value; }
         });
+
+        // Core form data object model
+        Object.defineProperty(this, 'form_data', {
+            get: function() { return state.form_data; },
+            set: function(value) { state.form_data = value; }
+        });
+
+        /*** Methods ***/
+
+        this.setFormData = function(value) {
+            merge(state.form_data, value);
+        }
+
+        this.setFormValue = function(property, value) {
+            merge(state.form_data, { [property]: value });
+        }
     });
 }
