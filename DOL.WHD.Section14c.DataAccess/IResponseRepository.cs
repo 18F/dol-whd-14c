26fe50402d830ecd77a8a86420d09cf1d566ae69
Kyle Linden <kyle.linden@appliedis.com>
2016-10-11 13:58:57 -0400

    add endpoint for retrieving list of question responses

---
+++ DOL.WHD.Section14c.DataAccess/IResponseRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using DOL.WHD.Section14c.Domain.Models.Submission;
+
+namespace DOL.WHD.Section14c.DataAccess
+{
+    public interface IResponseRepository : IDisposable
+    {
+        IEnumerable<Response> GetResponses(string questionKey = null, bool onlyActive = true);
+    }
+}
