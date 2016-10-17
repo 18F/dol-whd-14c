5601a8b3f02780274e295e513913c88bd107ae0d
Kyle Linden <kyle.linden@appliedis.com>
2016-10-17 17:07:08 -0400

    add application info section

---
+++ DOL.WHD.Section14c.Web/src/modules/components/sectionAppInfo/sectionAppInfoDirective.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(ngModule) {
+  ngModule.directive('sectionAppInfo', function() {
+
+      'use strict';
+
+      return {
+          restrict: 'EA',
+          template: require('./sectionAppInfoTemplate.html'),
+          controller: 'sectionAppInfoController',
+          scope: { },
+          controllerAs: 'vm'
+      };
+  });
+}
