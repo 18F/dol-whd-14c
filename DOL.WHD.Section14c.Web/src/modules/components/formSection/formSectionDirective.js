ceca24584abbdac8bb6b19dfb2701d1bf3fae496
Jeff Smith <jeffsmith@gmail.com>
2016-10-17 13:07:40 -0400

    Added custom directives for help links, and added help links and sidebar links to Wage Data form.

--- DOL.WHD.Section14c.Web/src/modules/components/formSection/formSectionDirective.js
+++ DOL.WHD.Section14c.Web/src/modules/components/formSection/formSectionDirective.js
@@ -12,4 +12,34 @@ module.exports = function(ngModule) {
             scope: { }
         };
     });
+
+    ngModule.directive('helplink', function() {
+        'use strict'
+
+        return {
+            template: '<div class="help-link">?</div>',
+            replace: true,
+            link: function(scope, element, attrs) {
+                element.bind('click', function() {
+                    console.log("clicked");
+                    element.next().toggleClass('show');
+                })
+            }
+        }
+    });
+
+    ngModule.directive('helptext', function() {
+        'use strict'
+
+        return {
+            transclude: true,
+            template: '<div class="help-text"><ng-transclude></ng-transclude></div>',
+            replace: true,
+            link: function(scope, element, attrs) {
+                scope.$watch('showAllHelp', function() {
+                    element.toggleClass('show', scope.showAllHelp === true);
+                });
+            }
+        }
+    });
 }
