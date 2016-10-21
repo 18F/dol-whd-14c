6757139453464e12c111c6da21bb37afce8c6801
Matt Walker <matt.walker@appliedis.com>
2016-10-21 17:22:09 -0400

    https local updates

--- DOL.WHD.Section14c.Web/env.js
+++ DOL.WHD.Section14c.Web/env.js
@@ -2,7 +2,7 @@
     window.__env = window.__env || {};
 
     // base api url
-    window.__env.api_url = 'http://localhost:3334';
+    window.__env.api_url = 'https://localhost:44399';
 
     // Google reCaptcha Site Key
     window.__env.reCaptchaSiteKey = '6LeqeggUAAAAALC5zT4OHbDJk9gHNT0GGZbJMOnG';
