202a0b7e36be75ab52d1a6421a32b9cad4bab904
Matt Walker <matt.walker@appliedis.com>
2016-11-04 18:01:01 -0400

    account management unit tests

--- DOL.WHD.Section14c.Web/src/modules/components/accountManagementControls/accountCreateButtonController.test.js
+++ DOL.WHD.Section14c.Web/src/modules/components/accountManagementControls/accountCreateButtonController.test.js
@@ -2,8 +2,9 @@ describe('accountCreateButtonController', function() {
 
     beforeEach(module('14c'));
 
-    beforeEach(inject(function ($rootScope, $controller) {
+    beforeEach(inject(function ($rootScope, $controller, $location) {
         scope = $rootScope.$new();
+        mockLocation = $location;
 
         accountCreateButtonController = function() {
             return $controller('accountCreateButtonController', {
@@ -14,6 +15,13 @@ describe('accountCreateButtonController', function() {
 
     it('invoke controller', function() {
         var controller = accountCreateButtonController();
+    });
+
+    it('create account click takes you to /account/create', function() {
+        var controller = accountCreateButtonController();
+        controller.createAccountClick();
+        expect(mockLocation.path()).toBe("/account/create");
+
     });
 });
 
--- DOL.WHD.Section14c.Web/src/modules/components/accountManagementControls/accountFormDirective.test.js
+++ DOL.WHD.Section14c.Web/src/modules/components/accountManagementControls/accountFormDirective.test.js
@@ -1,16 +1,21 @@
-describe('accountGrid', function() {
+describe('accountForm', function() {
 
     beforeEach(module('14c'));
 
     var element, rootScope;
     beforeEach(function () {
-        element = angular.element('<account-grid/>');
+        element = angular.element('<account-form/>');
     	inject(function ($rootScope, $compile, _$q_, apiService) {
             $q = _$q_;
             rootScope = $rootScope;
-            responses = $q.defer();
             mockApiService = apiService;
-            spyOn(mockApiService, 'getAccounts').and.returnValue(responses.promise);
+
+            getAccount = $q.defer();
+            spyOn(mockApiService, 'getAccount').and.returnValue(getAccount.promise);
+
+            getRoles = $q.defer();
+            spyOn(mockApiService, 'getRoles').and.returnValue(getRoles.promise);
+
             $compile(element)(rootScope);
         });
     });
