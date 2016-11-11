c49b279bfc066a4fa496be961d7b48089fa2bc25
Matt Walker <matt.walker@appliedis.com>
2016-11-11 09:16:34 -0500

    refactor sectionWorkSitesController unit tests

--- DOL.WHD.Section14c.Web/src/modules/components/sectionWorkSites/sectionWorkSitesController.test.js
+++ DOL.WHD.Section14c.Web/src/modules/components/sectionWorkSites/sectionWorkSitesController.test.js
@@ -2,8 +2,9 @@ describe('sectionWorkSitesController', function() {
 
     beforeEach(module('14c'));
 
-    beforeEach(inject(function ($rootScope, $controller) {
+    beforeEach(inject(function ($rootScope, $controller, validationService) {
         scope = $rootScope.$new();
+        mockValidationService = validationService;
         scope.responses = {
             "PrimaryDisability": [
                 {
@@ -27,68 +28,121 @@ describe('sectionWorkSitesController', function() {
         };
     }));
 
-    it('invoke controller', function() {
-        var controller = sectionWorkSitesController();
-    });
-
     it('sectionWorkSitesController has clearActiveWorker', function() {
         var controller = sectionWorkSitesController();
         controller.clearActiveWorker();
+
+        expect(controller.activeWorker).toEqual({});
+        expect(controller.activeWorkerIndex).toBe(-1);
     });
 
     it('sectionWorkSitesController has addEmployee', function() {
         var controller = sectionWorkSitesController();
+        controller.activeWorker = 'value';
+        spyOn(controller, 'clearActiveWorker')
         controller.addEmployee();
+
+        expect(controller.activeWorksite.employees.length).toBe(1)
+        expect(controller.activeWorksite.employees[0]).toBe('value')
+        expect(controller.clearActiveWorker).toHaveBeenCalled();
     });
+
     it('sectionWorkSitesController has doneAddingEmployees', function() {
         var controller = sectionWorkSitesController();
         controller.doneAddingEmployees();
+
+        expect(controller.addingEmployee).toBe(false);
     });
+
     it('sectionWorkSitesController has editEmployee', function() {
         var controller = sectionWorkSitesController();
-        controller.activeWorksite = { employees: [] }
-        controller.editEmployee();
+        controller.activeWorksite = { employees: [{id: 1}] }
+        controller.editEmployee(0);
+
+        expect(controller.activeWorker.id).toBe(1);
+        expect(controller.addingEmployee).toBe(true);
     });
+
     it('sectionWorkSitesController has addWorkSite', function() {
         var controller = sectionWorkSitesController();
+        spyOn(controller, 'setActiveTab')
         controller.addWorkSite();
+
+        expect(controller.addingWorkSite).toBe(true);
+        expect(controller.setActiveTab).toHaveBeenCalledWith(1);
     });
+
     it('sectionWorkSitesController has saveWorkSite', function() {
         var controller = sectionWorkSitesController();
+        spyOn(controller, 'clearActiveWorker')
+        spyOn(controller, 'clearActiveWorkSite')
+        controller.activeWorksite = {id: 1}
         controller.saveWorkSite();
+
+        expect(controller.clearActiveWorker).toHaveBeenCalled();
+        expect(controller.clearActiveWorkSite).toHaveBeenCalled();
+        expect(controller.addingWorkSite).toBe(false);                        
     });
+    
     it('sectionWorkSitesController has editWorkSite', function() {
         var controller = sectionWorkSitesController();
-        controller.editWorkSite();
+        scope.formData.workSites = [{id: 1}];
+        spyOn(controller, 'setActiveTab')
+        controller.editWorkSite(0);
+
+        expect(controller.addingWorkSite).toBe(true);   
+        expect(controller.setActiveTab).toHaveBeenCalledWith(1);
     });
+
     it('sectionWorkSitesController has deleteWorkSite', function() {
         var controller = sectionWorkSitesController();
-        controller.deleteWorkSite();
+        scope.formData.workSites = [{id: 1}];
+        controller.deleteWorkSite(0);
+
+        expect(scope.formData.workSites.length).toBe(0);   
     });
+
     it('sectionWorkSitesController has setActiveTab', function() {
         var controller = sectionWorkSitesController();
-        controller.setActiveTab();
-        controller.setActiveTab(2);
-    });
-    it('sectionWorkSitesController has siteRowClicked', function() {
+        controller.setActiveTab(1);
+
+        expect(controller.activeTab).toBe(1);
+    }); 
+
+    it('sectionWorkSitesController has getDisabilityDisplay, no employee passed', function() {
         var controller = sectionWorkSitesController();
-        controller.siteRowClicked({ target: '' });
+        var results = controller.getDisabilityDisplay();
+
+        expect(results).toBe(undefined);
     });
-    it('sectionWorkSitesController has getDisabilityDisplay', function() {
-        var controller = sectionWorkSitesController();
-        controller.getDisabilityDisplay();
-    });
+
+    //TODO getDisabilityDisplay assertions
+
     it('sectionWorkSitesController has validateActiveWorksiteProperty', function() {
         var controller = sectionWorkSitesController();
-        controller.validateActiveWorksiteProperty();
+        spyOn(mockValidationService, 'getValidationErrors');
+        controller.activeWorksiteIndex = 0;
+        controller.validateActiveWorksiteProperty(1);
+
+        expect(mockValidationService.getValidationErrors).toHaveBeenCalledWith('workSites[0].1');
     });
+
     it('sectionWorkSitesController has validateActiveWorkerProperty', function() {
         var controller = sectionWorkSitesController();
-        controller.validateActiveWorkerProperty();
+        spyOn(mockValidationService, 'getValidationErrors');
+        controller.activeWorksiteIndex = 0;        
+        controller.activeWorkerIndex = 0;
+        controller.validateActiveWorkerProperty(1);
+
+        expect(mockValidationService.getValidationErrors).toHaveBeenCalledWith('workSites[0].employees[0].1');
     });
     it('sectionWorkSitesController has validateActiveWorksiteWorker', function() {
         var controller = sectionWorkSitesController();
-        controller.validateActiveWorksiteWorker();
+        spyOn(mockValidationService, 'getValidationErrors');
+        controller.activeWorksiteIndex = 0;          
+        controller.validateActiveWorksiteWorker(1);
+
+        expect(mockValidationService.getValidationErrors).toHaveBeenCalledWith('workSites[0].employees[1]');
     });
 
     // test add/edit/remove workers and worksites
@@ -131,20 +185,4 @@ describe('sectionWorkSitesController', function() {
         expect(scope.formData.workSites.length).toBe(0);
     });
 
-    it('should get employee disability', function() {
-        var controller = sectionWorkSitesController();
-        var employee = { "primaryDisability": 32 };
-    });
-
-    it('should validate worksite and workery properties', function() {
-        var controller = sectionWorkSitesController();
-
-        controller.activeWorksiteIndex = 0;
-        controller.validateActiveWorksiteProperty("name");
-
-        controller.activeWorkerIndex = 0;
-        controller.validateActiveWorkerProperty("name");
-
-        controller.validateActiveWorksiteWorker(0);
-    })
 });
