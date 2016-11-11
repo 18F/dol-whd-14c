430f948353e4623a780d03a72db58207217a4687
Matt Walker <matt.walker@appliedis.com>
2016-11-10 19:05:31 -0500

    refactor sectionEmployerController

--- DOL.WHD.Section14c.Web/src/modules/components/sectionEmployer/sectionEmployerController.test.js
+++ DOL.WHD.Section14c.Web/src/modules/components/sectionEmployer/sectionEmployerController.test.js
@@ -14,7 +14,34 @@ describe('sectionEmployerController', function() {
         };
     }));
 
-    it('invoke controller', function() {
+    it('onHasTradeNameChange', function() {
         var controller = sectionEmployerController();
+        scope.formData.employer.tradeName = 'value';
+        controller.onHasTradeNameChange();
+
+        expect(scope.formData.employer.tradeName).toBe('');
     });
+    it('onHasLegalNameChange', function() {
+        var controller = sectionEmployerController();
+        scope.formData.employer.priorLegalName = 'value';
+        controller.onHasLegalNameChange();
+
+        expect(scope.formData.employer.priorLegalName).toBe('');
+    });
+
+    it('toggleDeductionType toggle on', function() {
+        var controller = sectionEmployerController();
+        scope.formData.employer.providingFacilitiesDeductionTypeId = [];
+        controller.toggleDeductionType(1);
+
+        expect(scope.formData.employer.providingFacilitiesDeductionTypeId.indexOf(1)).toBe(0)
+    });
+
+    it('toggleDeductionType toggle off', function() {
+        var controller = sectionEmployerController();
+        scope.formData.employer.providingFacilitiesDeductionTypeId = [1];
+        controller.toggleDeductionType(1);
+
+        expect(scope.formData.employer.providingFacilitiesDeductionTypeId.indexOf(1)).toBe(-1)
+    });          
 });
