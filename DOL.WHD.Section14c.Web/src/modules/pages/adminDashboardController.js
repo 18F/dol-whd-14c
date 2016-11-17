51ca2cab7643d074a2b273bfc5e9315844b2401c
Jeff Smith <jeffsmith@gmail.com>
2016-11-17 15:38:52 -0500

    More user management work.

--- DOL.WHD.Section14c.Web/src/modules/pages/adminDashboardController.js
+++ DOL.WHD.Section14c.Web/src/modules/pages/adminDashboardController.js
@@ -1,10 +1,14 @@
 'use strict';
 
 module.exports = function(ngModule) {
-  ngModule.controller('adminDashboardController', function($scope, stateService) {
+  ngModule.controller('adminDashboardController', function($scope, $location, stateService) {
       'ngInject';
       'use strict';
 
       $scope.appList = stateService.appList;
+
+      $scope.gotoUsers = function() {
+          $location.path("/admin/users");
+      }
   });
 }
