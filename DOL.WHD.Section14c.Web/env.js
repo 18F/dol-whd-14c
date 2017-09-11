588839a4270564dd13880c7b0bd6f593fc45b233
Brendan Sudol <brendan.sudol@gsa.gov>
2017-09-11 16:44:00 -0400

    add pa11y

--- DOL.WHD.Section14c.Web/env.js
+++ DOL.WHD.Section14c.Web/env.js
@@ -1,8 +1,8 @@
 (function(window) {
   window.__env = window.__env || {};
   Object.assign(window.__env, {
-    api_url: 'https://dol-whd-section14c-api-stg.azurewebsites.net',
-    reCaptchaSiteKey: '6Lc7OA4UAAAAADghnfWwelZ6XPtBUDAnywKtW0xl',
+    api_url: 'https://localhost:44399',
+    reCaptchaSiteKey: '6LeqeggUAAAAALC5zT4OHbDJk9gHNT0GGZbJMOnG',
     requireHttps: true,
     tokenCookieDurationMinutes: 20160
   });
