3337d113d836ad056e4fb65bdc030bcea50c8c1b
Brendan Sudol <brendan.sudol@gsa.gov>
2017-09-26 12:44:09 -0400

    question help accessibility

--- DOL.WHD.Section14c.Web/src/modules/components/formSection/formSectionDirective.js
+++ DOL.WHD.Section14c.Web/src/modules/components/formSection/formSectionDirective.js
@@ -16,7 +16,7 @@ module.exports = function(ngModule) {
     'use strict';
 
     return {
-      template: '<div class="help-link">?</div>',
+      template: '<button type="button" class="help-link">?</button>',
       replace: true,
       link: function(scope, element, attrs) {
         element.bind('click', function() {
