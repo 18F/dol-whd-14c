497106e5afb6022e63cf44da86419485dabe9dcd
Harun Davood <davoodharun>
2017-10-05 18:33:55 +0000

    removed embedded resource

--- DOL.WHD.Section14c.DataAccess/Properties/Resources.Designer.cs
+++ DOL.WHD.Section14c.DataAccess/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace DOL.WHD.Section14c.DataAccess.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] licenses {
-            get {
-                object obj = ResourceManager.GetObject("licenses", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
     }
 }
