fc5d924a532829467952b542a003367cdcad61a7
Harun Davood <davoodharun@gmail.com>
2017-10-06 20:07:51 -0400

    added replace npm package to handle double text replacement of env.js api_url for web deploy

--- DOL.WHD.Section14c.Web/env.js
+++ DOL.WHD.Section14c.Web/env.js
@@ -1,7 +1,7 @@
 (function(window) {
   window.__env = window.__env || {};
   Object.assign(window.__env, {
-    api_url: 'https://localhost:44399',
+    api_url: null || 'https://localhost:44399', // DO NOT CHANGE null VALUE -- this is necessary for web deploy
     reCaptchaSiteKey: '6LeqeggUAAAAALC5zT4OHbDJk9gHNT0GGZbJMOnG',
     requireHttps: true,
     tokenCookieDurationMinutes: 20160
