6ec826e6c2be190e967f5b3f839d4a6b719eecf3
Harun Davood <davoodharun@gmail.com>
2017-10-08 21:25:33 -0400

    added web deploy configuration for recaptcha

--- DOL.WHD.Section14c.Web/env.js
+++ DOL.WHD.Section14c.Web/env.js
@@ -1,8 +1,9 @@
 (function(window) {
   window.__env = window.__env || {};
   Object.assign(window.__env, {
-    api_url: null || 'https://localhost:44399', // DO NOT CHANGE null VALUE -- this is necessary for web deploy
-    reCaptchaSiteKey: '6LeqeggUAAAAALC5zT4OHbDJk9gHNT0GGZbJMOnG',
+    // DO NOT CHANGE NULL VALUES -- these are necessary for web deploy
+    api_url: null || 'https://localhost:44399',
+    reCaptchaSiteKey: null || '6LeqeggUAAAAALC5zT4OHbDJk9gHNT0GGZbJMOnG',
     requireHttps: true,
     tokenCookieDurationMinutes: 20160
   });
