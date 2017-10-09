b7ad1ee6ce94d689a07b9bf0e71dfc04956f6d5f
Harun Davood <davoodharun@gmail.com>
2017-10-09 09:44:58 -0400

    removed replace npm package

--- DOL.WHD.Section14c.Web/env.js
+++ DOL.WHD.Section14c.Web/env.js
@@ -1,9 +1,8 @@
 (function(window) {
   window.__env = window.__env || {};
   Object.assign(window.__env, {
-    // DO NOT CHANGE NULL VALUES -- these are necessary for web deploy
-    api_url: null || 'https://localhost:44399',
-    reCaptchaSiteKey: null || '6LeqeggUAAAAALC5zT4OHbDJk9gHNT0GGZbJMOnG',
+    api_url: 'https://localhost:44399',
+    reCaptchaSiteKey: '6LeqeggUAAAAALC5zT4OHbDJk9gHNT0GGZbJMOnG',
     requireHttps: true,
     tokenCookieDurationMinutes: 20160
   });
