ba3395e5bcbc06e79a7229cb0a53593b0a6bb825
Harun Davood <davoodharun@gmail.com>
2017-10-10 03:44:20 -0400

    added comments

--- DOL.WHD.Section14c.Web/env.js
+++ DOL.WHD.Section14c.Web/env.js
@@ -1,3 +1,4 @@
+// This file is only used for local developement. If you edit anything besides existing environment variables, you must also edit ./src/deploy/env.js for deployment
 (function(window) {
   window.__env = window.__env || {};
   Object.assign(window.__env, {
