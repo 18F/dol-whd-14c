0579e95d70625d03e724a3a69da411fcd1c511d6
David.Cao <dciceman@yahoo.com>
2017-10-12 12:43:18 -0400

    Added missing files

---
+++ DOL.WHD.Section14c.Log/App_Start/DependencyResolutionConfig.cs
@@ -0,0 +1,33 @@
+﻿using DOL.WHD.Section14c.Log.Repositories;
+using System;
+using System.Collections.Generic;
+using SimpleInjector;
+using System.Linq;
+using System.Web;
+using SimpleInjector.Integration.WebApi;
+using System.Web.Http;
+
+namespace DOL.WHD.Section14c.Log
+{
+    public class DependencyResolutionConfig
+    {
+        public static void Register()
+        {
+            // Create the container as usual.
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebApiRequestLifestyle();
+            
+
+            // This is an extension method from the integration package.
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+
+
+            container.Register<IActivityLogRepository, ActivityLogRepository>(Lifestyle.Scoped);
+            container.Register<IErrorLogRepository, ErrorLogRepository>(Lifestyle.Scoped);
+
+            container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+        }
+    }
+}
