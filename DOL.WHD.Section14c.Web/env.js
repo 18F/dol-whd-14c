f66aee80a7552b03f9a1011d0ffd318e1745464d
Harun Davood <davoodharun@gmail.com>
2017-10-16 13:37:29 -0400

    aria labels for form navigation header

--- DOL.WHD.Section14c.Web/env.js
+++ DOL.WHD.Section14c.Web/env.js
@@ -2,8 +2,8 @@
 (function(window) {
     window.__env = window.__env || {};
     Object.assign(window.__env, {
-        api_url: 'https://localhost:44399',
-        reCaptchaSiteKey: '6LeqeggUAAAAALC5zT4OHbDJk9gHNT0GGZbJMOnG',
+        api_url: 'https://dol-whd-section14c-api-int.eastus.cloudapp.azure.com',
+        reCaptchaSiteKey: '6Lc7OA4UAAAAADghnfWwelZ6XPtBUDAnywKtW0xl',
         requireHttps: true,
         tokenCookieDurationMinutes: 20160
     });
--- DOL.WHD.Section14c.Web/src/modules/components/mainNavigationControl/mainNavigationControlController.js
+++ DOL.WHD.Section14c.Web/src/modules/components/mainNavigationControl/mainNavigationControlController.js
@@ -13,6 +13,15 @@ module.exports = function(ngModule) {
     'use strict';
 
     var vm = this;
+    vm.sections = navService.getSections().map(function(element){
+      console.log(element);
+      if(element.display === "WIOA") {
+        element.ariaLabel = "Workforce Innovation and Opportunity Act"
+      } else {
+        element.ariaLabel = element.display;
+      }
+      return element;
+    });
     vm.stateService = stateService;
     vm.navService = navService;
     vm.current = $route.current.params.section_id;
@@ -26,5 +35,20 @@ module.exports = function(ngModule) {
     this.onKeyPress = e => {
       if (e.which === 13) this.onNavClick(e);
     };
+
+    this.getSections = function() {
+      var sections = vm.navService.getSections();
+      sections = sections.map(function(element){
+        if(element.display === "WOIA") {
+          element.ariaLabel = "Workforce Innovation and Opportunity Act"
+        } else {
+          element.ariaLabel = element.display;
+        }
+        return element;
+      });
+
+      return sections;
+    }
+
   });
 };
