a610f2a490d8e60daaa4768d87e88b4032aefb1b
David.Cao <dciceman@yahoo.com>
2017-10-16 16:57:55 -0400

    Added Error Handler to return Log ID.

---
+++ DOL.WHD.Section14c.Log/Helpers/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DOL.WHD.Section14c.Log
+{
+    public class Constants
+    {
+        public const string NLogLoggerName = "NLog";
+
+        public const string CorrelationId = "CorrelationId";
+
+        public const string EIN = "EIN";
+
+        public const string UserId = "UserId";
+
+        public const string UserName = "UserName";
+
+        public const string MSRequestContext = "MS_RequestContext";
+
+    }
+}
---
+++ DOL.WHD.Section14c.Log/LogHelper/BaseApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+
+namespace DOL.WHD.Section14c.Log.LogHelper
+{
+    public class BaseApiController : ApiController
+    {
+        protected void NotFound(string message)
+        {
+            throw new ApiDataException((int)HttpStatusCode.NotFound, message, HttpStatusCode.NotFound, "");
+        }
+
+        protected void BadRequest(string message)
+        {
+            throw new ApiDataException((int)HttpStatusCode.BadRequest, message, HttpStatusCode.BadRequest, "");
+        }
+    }
+}
