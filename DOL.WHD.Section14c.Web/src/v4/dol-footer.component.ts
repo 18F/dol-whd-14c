c210752b362ec49d394d38c21adcaaa86d0515b3
Leslee Sheu <leslee.sheu@appliedis.com>
2017-10-20 13:31:01 -0400

    #317: Fixed the ampersands in the footer links.

--- DOL.WHD.Section14c.Web/src/v4/dol-footer.component.ts
+++ DOL.WHD.Section14c.Web/src/v4/dol-footer.component.ts
@@ -47,20 +47,20 @@ import { Component } from '@angular/core';
             <li><a href="http://www.oig.dol.gov/">Office of Inspector General</a></li>
             <li><a href="http://www.dol.gov/dol/contact/contact-phonekeypersonnel.htm">Leadership Team</a></li>
             <li><a href="http://www.dol.gov/dol/contact/">Contact Us</a></li>
-            <li><a href="https://public.govdelivery.com/accounts/USDOL/subscriber/new?topic_id=USDOL_167" onclick="window.open('https://public.govdelivery.com/accounts/USDOL/subscriber/new?topic_id=USDOL_167&amp;pop=t', '', 'scrollbars=1,toolbar=0,menubar=0,resizable=1,width=800,height=420');return false;">Subscribe to the DOL Newsletter</a></li>
+            <li><a href="https://public.govdelivery.com/accounts/USDOL/subscriber/new?topic_id=USDOL_167" onclick="window.open('https://public.govdelivery.com/accounts/USDOL/subscriber/new?topic_id=USDOL_167&pop=t', '', 'scrollbars=1,toolbar=0,menubar=0,resizable=1,width=800,height=420');return false;">Subscribe to the DOL Newsletter</a></li>
             <li><a href="http://www.dol.gov/_sec/newsletter/">Read The DOL Newsletter</a></li>
             <li><a href="http://www.dol.gov/easl/">Emergency Accountability Status Link</a></li>
           </ul>
           <ul class="usa-unstyled-list usa-width-one-fourth content-group" role="navigation">
             <li class="mb1"><strong>FEDERAL GOVERNMENT</strong></li>
-            <li><a href="http://www.dol.gov/cgi-bin/leave-dol.asp?exiturl=http://www.whitehouse.gov&amp;exitTitle=www.whitehouse.gov&amp;fedpage=yes">White House</a></li>
-            <li><a href="http://www.dol.gov/cgi-bin/leave-dol.asp?exiturl=https://www.healthcare.gov&amp;exitTitle=www.healthcare.gov&amp;fedpage=yes">Affordable Care Act</a></li>
+            <li><a href="http://www.dol.gov/cgi-bin/leave-dol.asp?exiturl=http://www.whitehouse.gov&exitTitle=www.whitehouse.gov&fedpage=yes">White House</a></li>
+            <li><a href="http://www.dol.gov/cgi-bin/leave-dol.asp?exiturl=https://www.healthcare.gov&exitTitle=www.healthcare.gov&fedpage=yes">Affordable Care Act</a></li>
             <li><a href="http://www.dol.gov/opa/disaster-recovery.htm">Disaster Recovery Assistance</a></li>
-            <li><a href="http://www.dol.gov/cgi-bin/leave-dol.asp?exiturl=http://www.usa.gov&amp;exitTitle=www.usa.gov&amp;fedpage=yes">USA.gov</a></li>
+            <li><a href="http://www.dol.gov/cgi-bin/leave-dol.asp?exiturl=http://www.usa.gov&exitTitle=www.usa.gov&fedpage=yes">USA.gov</a></li>
             <li><a href="http://www.dol.gov/dol/PlainWriting/">Plain Writing Act</a></li>
             <li><a href="http://www.dol.gov/recovery/">Recovery Act</a></li>
             <li><a href="http://www.dol.gov/oasam/programs/crc/NoFearResult.htm">No Fear Act</a></li>
-            <li><a href="http://www.dol.gov/cgi-bin/leave-dol.asp?exiturl=https://osc.gov/&amp;exitTitle=osc.gov&amp;fedpage=yes">U.S. Office of Special Counsel</a></li>
+            <li><a href="http://www.dol.gov/cgi-bin/leave-dol.asp?exiturl=https://osc.gov/&exitTitle=osc.gov&fedpage=yes">U.S. Office of Special Counsel</a></li>
           </ul>
         </div>
       </div>
