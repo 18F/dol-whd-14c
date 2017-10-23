2f8e40ac8f4eeecf925d43ee0a0467589f05f18e
Harun Davood <davoodharun@gmail.com>
2017-10-23 13:05:53 -0400

    Update CodeCoverage.bat

--- DOL.WHD.Section14c.Test/CodeCoverage.bat
+++ DOL.WHD.Section14c.Test/CodeCoverage.bat
@@ -1,9 +1,9 @@
 ﻿
 Pushd "%~dp0"
 
-C:\nuget.exe install TestRunner -Version 1.2.0 -OutputDirectory tools
-C:\nuget.exe install OpenCover -Version 4.6.519 -OutputDirectory tools
-C:\nuget.exe install ReportGenerator -Version 2.4.5 -OutputDirectory tools
+nuget install TestRunner -Version 1.2.0 -OutputDirectory tools
+nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
+nuget install ReportGenerator -Version 2.4.5 -OutputDirectory tools
 
 .\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\TestRunner.1.2.0\tools\TestRunner.exe -targetargs:".\bin\Debug\DOL.WHD.Section14c.Test.dll" -filter:"+[*]DOL.WHD.Section14c.* -[*.Tests]* -[*.Test]* -[*]DOL.WHD.Section14c.DataAccess.*" -register:user
 
