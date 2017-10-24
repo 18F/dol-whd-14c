4883fdccc27bebcc84577f2f7650965a30e30635
Harun Davood <davoodharun@gmail.com>
2017-10-24 10:54:34 -0400

    temporary fix for self signed certs for IE 11

--- DOL.WHD.Section14c.Web/src/modules/services/authService.js
+++ DOL.WHD.Section14c.Web/src/modules/services/authService.js
@@ -14,36 +14,40 @@ module.exports = function(ngModule) {
     this.userLogin = function(email, password) {
       const self = this;
       const url = _env.api_url + '/Token';
+      const mockUrl = _env.api_url + '/'; // Temporary fix for IE 11
       const d = $q.defer();
-
       $http({
-        method: 'POST',
-        url: url,
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        data: $.param({
-          grant_type: 'password',
-          userName: email,
-          password: password
-        })
-      }).then(
-        function successCallback(result) {
-          const data = result.data;
-          stateService.access_token = data.access_token;
-          self.authenticateUser().then(
-            function() {
-              d.resolve();
-            },
-            function(error) {
-              d.reject(error);
-            }
-          );
-        },
-        function errorCallback(error) {
-          //console.log(error);
-          d.reject(error);
-        }
-      );
-
+        method: 'GET',
+        url: mockUrl
+      }).then(function(){
+        $http({
+          method: 'POST',
+          url: url,
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          data: $.param({
+            grant_type: 'password',
+            userName: email,
+            password: password
+          })
+        }).then(
+          function successCallback(result) {
+            const data = result.data;
+            stateService.access_token = data.access_token;
+            self.authenticateUser().then(
+              function() {
+                d.resolve();
+              },
+              function(error) {
+                d.reject(error);
+              }
+            );
+          },
+          function errorCallback(error) {
+            //console.log(error);
+            d.reject(error);
+          }
+        );
+      })
       return d.promise;
     };
 
