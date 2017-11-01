222366532da40a6297f44bf0cdc116f921a7bc53
Harun Davood <davoodharun@gmail.com>
2017-11-01 10:34:17 -0400

    untracks changes to env.js

---
+++ DOL.WHD.Section14c.Web/env.js
@@ -0,0 +1,9 @@
+// This file is only used for local developement. If you edit anything besides existing environment variables, you must also edit ./src/deploy/env.js for deployment
+(function(window) {
+    window.__env = window.__env || {};
+    Object.assign(window.__env, {
+        api_url: 'https://localhost:44399',        
+        requireHttps: true,
+        tokenCookieDurationMinutes: 20160
+    });
+})(this);
