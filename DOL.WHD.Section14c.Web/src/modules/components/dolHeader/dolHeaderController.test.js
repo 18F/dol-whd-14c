1687e7674420c779654993e29f0075da3ee60dad
Harun Davood <davoodharun@gmail.com>
2017-11-02 12:52:42 -0400

    fixed testing for dolHeader

--- DOL.WHD.Section14c.Web/src/modules/components/dolHeader/dolHeaderController.test.js
+++ DOL.WHD.Section14c.Web/src/modules/components/dolHeader/dolHeaderController.test.js
@@ -1,4 +1,4 @@
-describe('formFooterControlsController', function() {
+describe('dolHeaderController', function() {
   beforeEach(module('14c'));
 
   beforeEach(
@@ -11,91 +11,8 @@ describe('formFooterControlsController', function() {
       autoSaveService
     ) {
       scope = $rootScope.$new();
-      $q = _$q_;
-
-      mockNavService = navService;
-      mockApiService = apiService;
-      mockAutoSaveService = autoSaveService;
-
-      spyOn(mockNavService, 'hasNext');
-      spyOn(mockNavService, 'hasBack');
-      spyOn(mockNavService, 'getNextSection');
-
-      saveApplication = $q.defer();
-      spyOn(mockApiService, 'saveApplication').and.returnValue(
-        saveApplication.promise
-      );
-
-      formFooterControlsController = function() {
-        return $controller('formFooterControlsController', {
-          $scope: scope,
-          navService: mockNavService
-        });
-      };
+      
     })
   );
 
-  it('save', function() {
-    var controller = formFooterControlsController();
-    spyOn(mockAutoSaveService, 'save');
-    controller.doSave();
-    scope.$apply();
-
-    expect(mockAutoSaveService.save).toHaveBeenCalled();
-  });
-
-  it('onNextClick no next', function() {
-    var controller = formFooterControlsController();
-    spyOn(controller, 'doSave');
-    spyOn(mockNavService, 'goNext');
-    controller.onNextClick();
-    scope.$apply();
-
-    expect(controller.doSave).toHaveBeenCalled();
-    expect(mockNavService.goNext).not.toHaveBeenCalled();
-  });
-
-  it('onNextClick', function() {
-    var controller = formFooterControlsController();
-    controller.hasNext = true;
-    spyOn(controller, 'doSave');
-    spyOn(mockNavService, 'goNext');
-    controller.onNextClick();
-    scope.$apply();
-
-    expect(controller.doSave).toHaveBeenCalled();
-    expect(mockNavService.goNext).toHaveBeenCalled();
-  });
-
-  it('onBackClick no back', function() {
-    var controller = formFooterControlsController();
-    spyOn(controller, 'doSave');
-    spyOn(mockNavService, 'goBack');
-    controller.onBackClick();
-    scope.$apply();
-
-    expect(controller.doSave).toHaveBeenCalled();
-    expect(mockNavService.goBack).not.toHaveBeenCalled();
-  });
-
-  it('onBackClick', function() {
-    var controller = formFooterControlsController();
-    controller.hasBack = true;
-    spyOn(controller, 'doSave');
-    spyOn(mockNavService, 'goBack');
-    controller.onBackClick();
-    scope.$apply();
-
-    expect(controller.doSave).toHaveBeenCalled();
-    expect(mockNavService.goBack).toHaveBeenCalled();
-  });
-
-  it('onSaveClick', function() {
-    var controller = formFooterControlsController();
-    spyOn(controller, 'doSave');
-    controller.onSaveClick();
-    scope.$apply();
-
-    expect(controller.doSave).toHaveBeenCalled();
-  });
 });
--- DOL.WHD.Section14c.Web/src/modules/components/dolHeader/dolHeaderDirective.test.js
+++ DOL.WHD.Section14c.Web/src/modules/components/dolHeader/dolHeaderDirective.test.js
@@ -1,9 +1,9 @@
-describe('formFooterControls', function() {
+describe('dolHeader', function() {
   beforeEach(module('14c'));
 
   var element, rootScope;
   beforeEach(function() {
-    element = angular.element('<form-footer-controls/>');
+    element = angular.element('<dol-header/>');
     inject(function($rootScope, $compile) {
       rootScope = $rootScope;
       //$compile(element)(rootScope);
