b0cfb4b1d0234cd3332f70b950978d8d242db67f
Harun Davood <davoodharun@gmail.com>
2017-11-03 00:52:23 -0400

    removed console logs

--- DOL.WHD.Section14c.Web/src/modules/components/anchorLink/anchorLinkDirective.js
+++ DOL.WHD.Section14c.Web/src/modules/components/anchorLink/anchorLinkDirective.js
@@ -9,9 +9,7 @@ module.exports = function(ngModule) {
       replace: false,
       link: function(scope, element, attr) {
         element.bind('click', function() {
-          console.log(attr.scrollto)
           $anchorScroll(attr.scrollto);
-          console.log($document[0])
           $document[0].getElementById(attr.scrollto).focus();
         });
       },
