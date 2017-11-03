a17bd19a631baf31cb7f2edda213eef89857c5f0
Harun Davood <davoodharun@gmail.com>
2017-11-03 02:16:06 -0400

    added v4 and AJS notification modules

--- DOL.WHD.Section14c.Web/src/v4/error-handler.ts
+++ DOL.WHD.Section14c.Web/src/v4/error-handler.ts
@@ -3,11 +3,13 @@ import { ErrorHandler, Injectable, Injector} from '@angular/core';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { LoggingService } from './services/logging.service';
 import { customError } from './../models/customError';
+
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
   constructor(private injector: Injector, private loggingService: LoggingService) { }
-  handleError(error: customError) {
-        this.loggingService.addLog(error);
-        throw error;
-    }
+  handleError(error: any) {
+    var exception = new customError(error, "Error");
+    this.loggingService.addLog(error);
+    throw error;
+  }
 }
