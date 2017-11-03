ea02b3bfbb3a396c708452fe50ee55bfdaabb325
Harun Davood <davoodharun@gmail.com>
2017-11-03 01:02:03 -0400

    fixes click on keypress

--- DOL.WHD.Section14c.Web/src/modules/components/anchorLink/anchorLinkDirective.js
+++ DOL.WHD.Section14c.Web/src/modules/components/anchorLink/anchorLinkDirective.js
@@ -5,10 +5,14 @@ module.exports = function(ngModule) {
     'use strict';
 
     return {
-      template: '<a href scrollTo={{anchor}} class="dol-skip-to-main">skip to main content</a>',
+      template: '<a href role="button" scrollTo={{anchor}} class="dol-skip-to-main">skip to main content</a>',
       replace: false,
       link: function(scope, element, attr) {
         element.bind('click', function() {
+          $anchorScroll(attr.scrollto);
+          $document[0].getElementById(attr.scrollto).focus();
+        });
+        element.bind('keydown keypress', function() {
           $anchorScroll(attr.scrollto);
           $document[0].getElementById(attr.scrollto).focus();
         });
