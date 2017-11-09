7e67656c14d07e497409ec7000dd46a6c3348426
Harun Davood <davoodharun@gmail.com>
2017-11-09 16:32:27 -0500

    added directive to template

--- DOL.WHD.Section14c.Web/src/modules/components/sectionWorkSites/index.js
+++ DOL.WHD.Section14c.Web/src/modules/components/sectionWorkSites/index.js
@@ -3,4 +3,5 @@
 module.exports = function(ngModule) {
   require('./sectionWorkSitesController')(ngModule);
   require('./sectionWorkSitesDirective')(ngModule);
+  require('./employeeWageDataTableDirective')(ngModule);  
 };
