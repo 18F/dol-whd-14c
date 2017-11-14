5cddb8498236fbb2e88b9bf550d91b5f57c24499
dciceman <dciceman@yahoo.com>
2017-11-14 00:24:10 -0500

    Updated Email Api and UnitTest

--- DOL.WHD.Section14c.EmailApi/Business/Services/EmailService.cs
+++ DOL.WHD.Section14c.EmailApi/Business/Services/EmailService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Mail;
 using DOL.WHD.Section14c.Log.LogHelper;
 using System.Net;
+using System.IO;
 
 namespace DOL.WHD.Section14c.EmailApi.Business
 {
@@ -12,13 +13,13 @@ namespace DOL.WHD.Section14c.EmailApi.Business
     {
         private SmtpClient _smtpClient;
 
-        ///// <summary>
-        ///// Set SmtpClient
-        ///// </summary>
-        //public EmailService(SmtpClient smtpClient)
-        //{
-        //    _smtpClient = smtpClient ?? new SmtpClient();
-        //}
+        /// <summary>
+        /// Set SmtpClient
+        /// </summary>
+        public EmailService(SmtpClient smtpClient)
+        {
+            _smtpClient = smtpClient ?? new SmtpClient();
+        }
 
         /// <summary>
         /// Send Email
@@ -28,40 +29,46 @@ namespace DOL.WHD.Section14c.EmailApi.Business
         public bool SendEmail(EmailContent emailContent)
         {
             bool success = true;
-
             try
             {
                 if (emailContent == null)
                     throw new ArgumentNullException("Email Content Exception", "Send Email");
-                _smtpClient = new SmtpClient();
 
-                MailMessage mailMessage = new MailMessage();
-
-                // Allow multiple Recipients with MailMessage
-                foreach (var address in emailContent.To.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries))
+                using (MailMessage mailMessage = new MailMessage())
                 {
-                    mailMessage.To.Add(address);
-                }
 
-                // Add Emaill CC
-                if (!string.IsNullOrEmpty(emailContent.CC))
-                {
-                    foreach (var address in emailContent.CC.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries))
+                    // Allow multiple Recipients with MailMessage
+                    foreach (var address in emailContent.To.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries))
                     {
-                        mailMessage.CC.Add(address);
+                        mailMessage.To.Add(address);
                     }
-                }
-                
-                // Add Attachments
-                foreach (var attachment in emailContent.attachments)
-                {
-                    mailMessage.Attachments.Add(attachment);
-                }
 
-                mailMessage.Subject = emailContent.Subject;
-                mailMessage.Body = emailContent.Body;
-                
-                _smtpClient.Send(mailMessage);
+                    // Add Emaill CC
+                    if (!string.IsNullOrEmpty(emailContent.CC))
+                    {
+                        foreach (var address in emailContent.CC.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries))
+                        {
+                            mailMessage.CC.Add(address);
+                        }
+                    }
+
+                    mailMessage.Subject = emailContent.Subject;
+                    mailMessage.Body = emailContent.Body;
+                    
+                    // Get Attachments
+                    foreach (var KeyValuePair in emailContent.attachments)
+                    {
+                        var fileName = KeyValuePair.Key;
+                        var buffer = KeyValuePair.Value;
+                        // Add Attachment
+                        if ( buffer.Length > 0)
+                        {
+                            var memoryStream = new MemoryStream(buffer);
+                            mailMessage.Attachments.Add(new Attachment(memoryStream, fileName));
+                        }
+                    }
+                    _smtpClient.Send(mailMessage);
+                }
             }
             catch (Exception ex)
             {
@@ -71,6 +78,11 @@ namespace DOL.WHD.Section14c.EmailApi.Business
                     throw new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, HttpStatusCode.InternalServerError);
                 }
                 throw;
+            }
+            finally
+            {
+                if(_smtpClient != null)
+                    _smtpClient.Dispose();
             }
 
             return success;
---
+++ DOL.WHD.Section14c.Test/Controllers/EmailControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DOL.WHD.Section14c.EmailApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DOL.WHD.Section14c.EmailApi.Business;
+using System.Net.Mail;
+using System.IO;
+using DOL.WHD.Section14c.EmailApi.Helper;
+using System.Web.Http.Results;
+using System.Net;
+using System.Web.Http;
+
+namespace DOL.WHD.Section14c.EmailApi.Controllers.Tests
+{
+    [TestClass()]
+    public class EmailControllerTests
+    {
+
+        private IEmailService _emailService;
+        private EmailController _emailController;
+        private EmailContent _emailContent;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            string testEmailPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\TestEmails"));
+            if (!System.IO.Directory.Exists(testEmailPath))
+                System.IO.Directory.CreateDirectory(testEmailPath);
+
+            SmtpClient client = new SmtpClient()
+            {
+                PickupDirectoryLocation = testEmailPath
+            };
+
+            _emailService = new EmailService(client);
+
+            _emailContent = new EmailContent();
+            _emailContent.To = "test@test.com;test2@test.com";
+            _emailContent.CC = "test3@test.com;test4@test.com";
+            _emailContent.Subject = "My Test Subject";
+            _emailContent.Body = "My Test Body";
+
+            var testFileContents = "test";
+            var data = Encoding.ASCII.GetBytes(testFileContents);
+            Dictionary<string, byte[]> attachments = new Dictionary<string, byte[]>();
+            attachments.Add("test.txt", data);
+            _emailContent.attachments = attachments;
+
+            _emailController = new EmailController(_emailService);
+        }
+
+        [TestMethod()]
+        public void SendEmailTest()
+        {
+            IHttpActionResult result = _emailController.SendEmail(_emailContent);
+            var contentResult = result as OkNegotiatedContentResult<Boolean>;
+            Assert.IsTrue(contentResult.Content);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SendEmail_NullException_Test()
+        {
+            var result = _emailController.SendEmail(null);
+        }
+    }
+}
