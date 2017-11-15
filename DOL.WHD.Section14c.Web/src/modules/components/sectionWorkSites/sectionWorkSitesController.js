cba6b0b195dd23c0cb1dfd2b7d36d8ed05a83ab7
Greg Walker <mgwalker@gmail.com>
2017-11-15 16:25:30 -0600

    consistent whitespace

--- DOL.WHD.Section14c.Web/src/modules/components/sectionWorkSites/sectionWorkSitesController.js
+++ DOL.WHD.Section14c.Web/src/modules/components/sectionWorkSites/sectionWorkSitesController.js
@@ -28,17 +28,17 @@ module.exports = function(ngModule) {
     let query = $location.search();
 
     $('.cd-btn').on('click', function(event){
-  		event.preventDefault();
+      event.preventDefault();
       vm.clearActiveWorker();
-  		$('.cd-panel').addClass('is-visible');
-  	});
-  	//clode the lateral panel
-  	$('.cd-panel').on('click', function(event){
-  		if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) {
-  			$('.cd-panel').removeClass('is-visible');
-  			event.preventDefault();
-  		}
-  	});
+      $('.cd-panel').addClass('is-visible');
+    });
+    //clode the lateral panel
+    $('.cd-panel').on('click', function(event){
+      if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) {
+        $('.cd-panel').removeClass('is-visible');
+        event.preventDefault();
+      }
+    });
     var vm = this;
     vm.activeTab = query.t ? query.t : 1;
     vm.activeWorksite = {};
