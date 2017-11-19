2adfc56be8aa8ba93895e7b0239d3adf878d08e8
dciceman <dciceman@yahoo.com>
2017-11-19 16:51:26 -0500

    Remove my test configuration

--- DOL.WHD.Section14c.Api/Controllers/ApplicationController.cs
+++ DOL.WHD.Section14c.Api/Controllers/ApplicationController.cs
@@ -270,8 +270,7 @@ namespace DOL.WHD.Section14c.Api.Controllers
         /// <returns></returns>
         [HttpGet]
         [Route("download")]
-        //[AuthorizeClaims(ApplicationClaimTypes.SubmitApplication, ApplicationClaimTypes.ViewAllApplications)]
-        [AllowAnonymous]
+        [AuthorizeClaims(ApplicationClaimTypes.SubmitApplication, ApplicationClaimTypes.ViewAllApplications)]
         public async Task<IHttpActionResult> DownloadApplicationDocument(Guid applicationId)
         {
             var responseMessage = Request.CreateResponse(HttpStatusCode.OK);
