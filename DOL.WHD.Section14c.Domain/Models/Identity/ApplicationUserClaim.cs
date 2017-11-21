c69984a7adb32d13a22e03ac926fd577c31fe7a6
dciceman <dciceman@yahoo.com>
2017-11-21 14:30:55 -0500

    Updated Database.

--- DOL.WHD.Section14c.Domain/Models/Identity/ApplicationUserClaim.cs
+++ DOL.WHD.Section14c.Domain/Models/Identity/ApplicationUserClaim.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 
 namespace DOL.WHD.Section14c.Domain.Models.Identity
 {
     public class ApplicationUserClaim : IdentityUserClaim<string>
     {
 
+        public DateTime LastModifiedAt { get; set; }
+
+        public DateTime CreatedAt { get; set; }
     }
 }
