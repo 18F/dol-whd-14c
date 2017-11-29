0ef6cd54d3a2bdb80a8cfc31c59776720ed1ea2f
Leslee Sheu <leslee.sheu@appliedis.com>
2017-11-29 11:25:04 -0500

    Code update per pull request review.

--- DOL.WHD.Section14c.Web/src/modules/components/sectionWorkSites/sectionWorkSitesController.js
+++ DOL.WHD.Section14c.Web/src/modules/components/sectionWorkSites/sectionWorkSitesController.js
@@ -333,8 +333,10 @@ module.exports = function(ngModule) {
     // close the panel
     function closeSlidingPanel() {
       $('.cd-panel').removeClass('is-visible');
-      panelTrigger.focus();
-      $('body').removeClass('cd-panel-open'); 
+      $('body').removeClass('cd-panel-open');
+      if (panelTrigger) {
+        panelTrigger.focus();        
+      }    
     }
     $(document).keydown(function(event) {
         // escape key
