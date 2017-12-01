4e8e6dec44b843f18ee8a58685d1e95a92ffff29
Myket <Steven.Myket@appliedis.com>
2017-12-01 14:29:59 -0500

    Added titles to pages

--- DOL.WHD.Section14c.Web/src/modules/components/mainNavigationControl/mainNavigationControlController.js
+++ DOL.WHD.Section14c.Web/src/modules/components/mainNavigationControl/mainNavigationControlController.js
@@ -16,7 +16,6 @@ module.exports = function(ngModule) {
       if(element.display === "WIOA") {
         element.ariaLabel = "Workforce Innovation and Opportunity Act"
       } else {
-        document.title = element.display;
         element.ariaLabel = element.display;
       }
       return element;
