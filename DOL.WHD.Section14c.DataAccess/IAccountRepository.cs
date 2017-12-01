e75873eaaacce40736964dfa5ee9eca1951da709
dciceman <dciceman@yahoo.com>
2017-12-01 15:50:05 -0500

    Added  Employer Api

---
+++ DOL.WHD.Section14c.DataAccess/IAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using DOL.WHD.Section14c.Domain.Models.Submission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOL.WHD.Section14c.DataAccess
+{
+    public interface IEmployerRepository : IDisposable
+    {
+        IEnumerable<Employer> Get();
+        Task<int> AddAsync(Employer employer);
+        Task<int> ModifyEmployer(Employer employer);
+        Task<int> SaveChangesAsync();
+    }
+}
