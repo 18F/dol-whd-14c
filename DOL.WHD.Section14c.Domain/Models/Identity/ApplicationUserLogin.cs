79d27c38f35d1e064ebb9589fb7145a693865d21
dciceman <dciceman@yahoo.com>
2017-12-01 21:20:45 -0500

    Added Auditing columns

--- DOL.WHD.Section14c.Domain/Models/Identity/ApplicationUserLogin.cs
+++ DOL.WHD.Section14c.Domain/Models/Identity/ApplicationUserLogin.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using DOL.WHD.Section14c.DataAccess;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DOL.WHD.Section14c.Domain.Models.Identity
 {
-    public class ApplicationUserLogin : IdentityUserLogin<string>
+    public class ApplicationUserLogin : IdentityUserLogin<string>, IAuditedEntity
     {
         public DateTime LastModifiedAt { get; set; }
 
         public DateTime CreatedAt { get; set; }
+
+        public string CreatedBy_Id { get; set; }
+
+        [ForeignKey("CreatedBy_Id")]
+        public ApplicationUser CreatedBy { get; set; }
+
+        public string LastModifiedBy_Id { get; set; }
+
+        [ForeignKey("LastModifiedBy_Id")]
+        public ApplicationUser LastModifiedBy { get; set; }
     }
 }
