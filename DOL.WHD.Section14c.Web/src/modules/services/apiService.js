a46aba204722986979084dbe7d19b37b6a76f3ec
Harun Davood <davoodharun>
2017-12-06 17:47:51 +0000

    work on front end lwf

--- DOL.WHD.Section14c.Web/src/modules/services/apiService.js
+++ DOL.WHD.Section14c.Web/src/modules/services/apiService.js
@@ -209,8 +209,10 @@ module.exports = function(ngModule) {
       return d.promise;
     };
 
-    this.saveApplication = function(access_token, ein, applicationData) {
-      let url = _env.api_url + '/api/save/' + ein;
+
+    this.saveApplication = function(access_token, ein, employerId, applicationData) {
+      console.log(access_token, ein, employerId, applicationData);
+      let url = _env.api_url + '/api/save/' + ein + '/' + employerId;
       let d = $q.defer();
 
       applicationData.saved = moment.utc();
@@ -237,8 +239,8 @@ module.exports = function(ngModule) {
       return d.promise;
     };
 
-    this.getApplication = function(access_token, ein) {
-      let url = _env.api_url + '/api/save/' + ein;
+    this.getApplication = function(access_token, ein, employerId) {
+      let url = _env.api_url + '/api/save/' + ein + '/' + employerId;
       let d = $q.defer();
 
       $http({
