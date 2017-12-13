7702fa1a8334094c7af08c03ed14162644ce3e1c
Harun Davood <davoodharun>
2017-12-13 15:48:21 +0000

    removes breadcrumbs from employer registration

--- DOL.WHD.Section14c.Web/src/modules/pages/userLoginPageController.js
+++ DOL.WHD.Section14c.Web/src/modules/pages/userLoginPageController.js
@@ -1,8 +1,38 @@
 'use strict';
 
 module.exports = function(ngModule) {
-  ngModule.controller('userLoginPageController', function() {
+  ngModule.controller('userLoginPageController', function($scope, $location, apiService) {
     'ngInject';
     'use strict';
+
+    var vm = this;
+
+	vm.emailVerificationUrl = $location.absUrl();
+    vm.emailVerificationCode = $location.search().code;
+    vm.emailVerificationUserId = $location.search().userId;
+    vm.isEmailVerificationRequest =
+      vm.emailVerificationCode !== undefined &&
+      vm.emailVerificationCode !== undefined;
+    console.log(vm.isEmailVerificationRequest);
+    if (vm.isEmailVerificationRequest) {
+      $location.search('code', null);
+      $location.search('userId', null);
+      apiService
+        .emailVerification(
+          vm.emailVerificationUserId,
+          vm.emailVerificationCode,
+          $scope.verifyResponse
+        )
+        .then(
+          function() {
+            vm.emailVerified = true;
+          },
+          function() {
+            vm.emailVerificationError = true;
+          }
+        );
+    }
   });
 };
+
+
