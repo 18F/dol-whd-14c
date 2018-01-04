149817a1fa7c078fc90a207d12d7a88e3ce92a02
Harun Davood <davoodharun@gmail.com>
2018-01-04 08:22:37 -0500

    added tests

--- DOL.WHD.Section14c.Web/src/modules/components/changePasswordForm/changePasswordFormController.test.js
+++ DOL.WHD.Section14c.Web/src/modules/components/changePasswordForm/changePasswordFormController.test.js
@@ -5,17 +5,19 @@ describe('changePasswordFormController', function() {
   beforeEach(module('14c'));
 
   beforeEach(
-    inject(function($rootScope, $controller, _$q_, apiService) {
+    inject(function($rootScope, $controller, _$q_, apiService, $location) {
       scope = $rootScope.$new();
       $q = _$q_;
       mockApiService = apiService;
-      mockStateService = { user: {} };
+      mockStateService = { user: {}, logOut: function(){}};
+      mockLocation = $location;
 
       changePasswordFormController = function() {
         return $controller('changePasswordFormController', {
           $scope: scope,
           apiService: mockApiService,
-          stateService: mockStateService
+          stateService: mockStateService,
+          $location: mockLocation
         });
       };
 
@@ -58,5 +60,17 @@ describe('changePasswordFormController', function() {
     changePassword.reject({ data: {} });
     scope.$apply();
     expect(controller.changePasswordError).toBe(true);
+  });
+
+  it('logout should work', function() {
+    var controller = changePasswordFormController();
+    var e = jasmine.createSpyObj('e', ['preventDefault']);
+    spyOn(mockLocation, 'path');
+    spyOn(mockStateService, 'logOut');
+    controller.logOut(e);
+
+    expect(mockStateService.logOut).toHaveBeenCalled();
+    expect(mockLocation.path).toHaveBeenCalledWith('/');
+    expect(document.title).toBe('DOL WHD Section 14(c)');
   });
 });
