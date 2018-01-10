1ad8ba90c4edb5aa35d6afea977cb766c979bda1
Harun Davood <davoodharun>
2018-01-10 18:12:51 +0000

    formats employer table

--- DOL.WHD.Section14c.Web/src/modules/pages/landingPageController.js
+++ DOL.WHD.Section14c.Web/src/modules/pages/landingPageController.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 module.exports = function(ngModule) {
   ngModule.controller('landingPageController', function(
     $scope,
@@ -65,46 +66,98 @@ module.exports = function(ngModule) {
         $scope.applicationList = [];
         result.data.organizations.forEach(function(element) {
           var organization = {
+            applicationId: element.applicationId,
             employerId: element.employer.ein,
             employerName: element.employer.legalName,
             createdAt: dateFilter(element.createdAt) + " at " + new Date(element.createdAt).toLocaleTimeString(),
             lastModifiedAt: dateFilter(element.lastModifiedAt) + " at " + new Date(element.lastModifiedAt).toLocaleTimeString(),
-            applicationStatus: element.applicationStatus.name
+            applicationStatus: element.applicationStatus.name,
+            employerAddress: element.employer.physicalAddress.streetAddress + " " + element.employer.physicalAddress.city + ", " + element.employer.physicalAddress.state + " " + element.employer.physicalAddress.zipCode
+          }
+
+          if(element.applicationStatus.name === "New") {
+            organization.action = "Start"
+          } 
+          else if (element.applicationStatus.name === "InProgress") {
+            organization.action = "Continue"
+          } else {
+            organization.action = "Download"
           }
           
           $scope.applicationList.push(organization);
         });
 
-        $scope.tableWidget = $('#EmployerTable').DataTable({
-          data: $scope.applicationList,
-          columns: [
-            { data: 'employerId', title: "EIN"},
-            { data: 'employerName', title: "Employer" },
-            { data: 'createdAt', title: "Created At" },
-            { data: 'lastModifiedAt',  title: "Last Modified"},
-            { data: 'applicationStatus',  title: "Status"},
-          ],
-          columnDefs: [
-            {
-                className: 'control',
-                orderable: false,
-                targets:   0
-            },
-            { responsivePriority: 1, targets: 0 },
-            { responsivePriority: 2, targets: 1 },
-            { responsivePriority: 3, targets: 2 }
-          ]
-        });
+        $scope.initDatatable();
       })
     }
 
-    $scope.init();
-    $('#EmployerTable').on('click', 'td', function ($event) {
+    $scope.initDatatable = function () {
+      $scope.tableWidget = $('#EmployerTable').DataTable({
+        data: $scope.applicationList,
+        responsive: {
+            details: {
+                type: "column",
+                target: 0,
+                display: $.fn.dataTable.Responsive.display.childRow
+            }
+        },
+        columns: [
+          {
+              "className": 'control',
+              "orderable": false,
+              "data":null,
+              "defaultContent": ""
+          },
+          { data: 'employerId', title: "EIN"},
+          { data: 'applicationId', title: "Application Id"},
+          { data: 'employerName', title: "Employer" },
+          { data: 'createdAt', title: "Created At" },
+          { data: 'lastModifiedAt',  title: "Last Modified"},
+          { data: 'applicationStatus',  title: "Status"},
+          { data: 'employerAddress',  title: "Address"},
+          { data: 'action', title: 'Action'}
+        ],
+        columnDefs: [
+          {
+              className: 'control',
+              orderable: false,
+              targets:   0
+          },
+          { responsivePriority: 1, targets: 1 },
+          { responsivePriority: 1, targets: 6 },
+          { responsivePriority: 1, targets: 3 },
+          {
+              className: 'action',
+              orderable: false,
+              responsivePriority: 1,
+              targets: -1,
+              render: function (data) {
+                  var button = "<button>" + data + "</button>"
+                  return button;
+              },
+          }
+        ]
+      });
+    }
+
+    $scope.refreshTable = function () {
+      if ($scope.tableWidget) {
+        $scope.tableWidget.destroy()
+        $scope.tableWidget=null
+      }
+      
+      setTimeout(() => $scope.initDatatable(),0)
+    }
+
+    
+    $('#EmployerTable').on('click', '.action', function ($event) {
         $event.preventDefault();
         var tr = $(this).closest('tr');
         var row = $scope.tableWidget.row( tr );
         $scope.onApplicationClick(row[0][0]);
     });
+
+    $scope.init();
 
   });
 };
