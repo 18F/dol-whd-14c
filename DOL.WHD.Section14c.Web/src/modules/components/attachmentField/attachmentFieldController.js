615155cf0d78ea0797a8037654a3378ea4e781d6
Harun Davood <davoodharun@gmail.com>
2018-01-12 12:17:07 -0500

    adds multimple file upload

--- DOL.WHD.Section14c.Web/src/modules/components/attachmentField/attachmentFieldController.js
+++ DOL.WHD.Section14c.Web/src/modules/components/attachmentField/attachmentFieldController.js
@@ -12,20 +12,35 @@ module.exports = function(ngModule) {
     var vm = this;
     vm.stateService = stateService;
     vm.apiService = apiService;
-
+    $scope.formData = stateService.formData;
+    if(!$scope.formData[$scope.modelPrefix][$scope.inputId]) {
+      stateService.formData[$scope.modelPrefix][$scope.inputId] = [];
+    }
     this.onAttachmentSelected = function(fileinput) {
+
       if (fileinput.files.length > 0) {
+
         apiService
           .uploadAttachment(
             stateService.access_token,
             stateService.ein,
-            fileinput.files[0]
+            fileinput.files[fileinput.files.length-1]
           )
           .then(
             function(result) {
-              $scope.attachmentId = result.data[0].id;
-              $scope.attachmentName = result.data[0].originalFileName;
+              var attachment = {};
+
+              attachment.attachmentId = result.data[0].id;
+              attachment.attachmentName = result.data[0].originalFileName;
               fileinput.value = '';
+              if($scope.inputId === "prScaWageDeterminationAttachments" || $scope.inputId === "hScaWageDeterminationAttachments") {
+                $scope.formData[$scope.modelPrefix][$scope.inputId].push(attachment);
+              } else {
+                if($scope.formData[$scope.modelPrefix][$scope.inputId][0]) {
+                  vm.deleteAttachment($scope.formData[$scope.modelPrefix][$scope.inputId][0].attachmentId);
+                }
+                $scope.formData[$scope.modelPrefix][$scope.inputId][0] = attachment;
+              }
             },
             function() {
               //TODO: Display error
@@ -40,13 +55,15 @@ module.exports = function(ngModule) {
         .deleteAttachment(stateService.access_token, stateService.ein, id)
         .then(
           function() {
-            $scope.attachmentId = undefined;
-            $scope.attachmentName = undefined;
+            $scope.formData[$scope.modelPrefix][$scope.inputId].forEach(function(element, index) {
+              if (element.attachmentId === id) {
+                $scope.formData[$scope.modelPrefix][$scope.inputId][index].attachmentId = undefined;
+                $scope.formData[$scope.modelPrefix][$scope.inputId][index].attachmentName = undefined;
+              }
+            });
           },
           function() {
             //TODO: Display error
-            $scope.attachmentId = undefined;
-            $scope.attachmentName = undefined;
           }
         );
     };
