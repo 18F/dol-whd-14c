69c4b6bcea9bf7144de89d74b79739c1aadcf26e
Myket <Steven.Myket@appliedis.com>
2018-01-12 10:34:38 -0500

    Set correct html for valid/invalid files

--- DOL.WHD.Section14c.Web/src/modules/components/attachmentField/attachmentFieldController.js
+++ DOL.WHD.Section14c.Web/src/modules/components/attachmentField/attachmentFieldController.js
@@ -12,8 +12,37 @@ module.exports = function(ngModule) {
     var vm = this;
     vm.stateService = stateService;
     vm.apiService = apiService;
+    vm.uploadError = {
+      status: false,
+      message: ''
+    }
+    
 
     this.onAttachmentSelected = function(fileinput) {
+      vm.uploadError.status = false;
+      vm.uploadError.message = '';
+      console.log(fileinput.files[0].size);
+      console.log(fileinput.files[0].name);
+
+      var ext = fileinput.files[0].name.split(".")[1];
+      var allowedFileTypes = ['pdf', 'jpg', 'jpeg', 'png', 'csv', 'PDF'];
+
+      console.log(ext);
+      console.log(allowedFileTypes.indexOf(ext));
+      console.log((fileinput.files[0].size / 1024) / 1000 > 5);
+
+      if(allowedFileTypes.indexOf(ext) < 0) {
+        vm.uploadError.status = true;
+        vm.uploadError.message = 'Invalid file type';
+      } 
+      
+      if ((fileinput.files[0].size / 1024) / 1000 > 5) {
+        vm.uploadError.status = true;
+        vm.uploadError.message = 'Invalid file size';
+        console.log('in fileSize if statement');
+        console.log($scope.attachmentId);
+      }
+
       if (fileinput.files.length > 0) {
         apiService
           .uploadAttachment(
@@ -26,10 +55,13 @@ module.exports = function(ngModule) {
               $scope.attachmentId = result.data[0].id;
               $scope.attachmentName = result.data[0].originalFileName;
               fileinput.value = '';
+              console.log('in then block');
             },
-            function() {
+            function(error) {
               //TODO: Display error
               fileinput.value = '';
+              vm.uploadError.message = error.statusMessage;
+              console.log('in api catch');
             }
           );
       }
