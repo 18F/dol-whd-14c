294c804a1025817a3d38931c749c87fb569d9a83
Harun Davood <davoodharun@gmail.com>
2018-01-15 14:04:24 -0500

    codeclimate

--- DOL.WHD.Section14c.Web/src/modules/components/userRegistrationForm/userRegistrationFormController.test.js
+++ DOL.WHD.Section14c.Web/src/modules/components/userRegistrationForm/userRegistrationFormController.test.js
@@ -86,15 +86,6 @@ describe('userRegistrationFormController', function() {
     expect(scope.passwordStrength.score).toBe(4);
   });
 
-  it('general error is displayed', function() {
-    var controller = userRegistrationFormController();
-    scope.onSubmitClick();
-    expect(controller.submittingForm).toBe(true);
-    userRegister.reject({});
-    scope.$apply();
-    expect(controller.generalRegistrationError).toBe(true);
-  });
-
   it('submitting registration has an error, no error data', function() {
     var controller = userRegistrationFormController();
     scope.onSubmitClick();
