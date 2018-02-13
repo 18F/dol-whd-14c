b7de3a214a41bfd799bb2f39fb7bc8456bdf0e97
Harun Davood <davoodharun@gmail.com>
2018-02-13 14:14:55 -0500

    idle timer configuration

--- DOL.WHD.Section14c.Web/src/modules/app.js
+++ DOL.WHD.Section14c.Web/src/modules/app.js
@@ -82,8 +82,9 @@ require('./pages')(app);
 require('./services')(app);
 app.config(function(IdleProvider, KeepaliveProvider) {
   // configure Idle settings
-  IdleProvider.idle(780); // in seconds
+  IdleProvider.idle(10); // in seconds
   IdleProvider.timeout(120); // in seconds
+  IdleProvider.autoResume(false);
   KeepaliveProvider.interval(2); // in seconds
 })
 let routeConfig = require('./routes.config');
