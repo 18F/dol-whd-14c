911360e1f3a8918cfb869404487075ea88e4b630
Joe B <36516128+joebhaktiarDOL@users.noreply.github.com>
2018-04-13 08:58:36 -0400

    Update env.js

--- DOL.WHD.Section14c.Web/env.js
+++ DOL.WHD.Section14c.Web/env.js
@@ -2,10 +2,8 @@
 (function(window) {
   window.__env = window.__env || {};
   Object.assign(window.__env, {
-    //api_url: 'https://localhost:44399',
-     api_url: 'http://localhost:3334/',
-      //api_url: 'https://dol-whd-section14c-api-int.aisdemos.com/',
-    requireHttps: true,
+    api_url: 'https://localhost:44399',
+     requireHttps: true,
     tokenCookieDurationMinutes: 20160,
     allowedFileTypes: '["pdf", "jpg", "JPG", "jpeg", "JPEG", "png", "PNG", "PDF"]',
     helpEmailAddress:'14c-help@dol.gov',
